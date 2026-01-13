cac44367431635390f7723edc1b5e08ed45b2534
Ege Telli <83495425+egetelli@users.noreply.github.com>
2026-01-13 11:23:00 +0300

    feature(register-endpoint): register endpoint eklendi (#2)

--- API/Program.cs
+++ API/Program.cs
@@ -1,14 +1,47 @@
+using System.Text;
 using API.Data;
+using API.Endpoints;
 using API.Modals;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
+//Uygulamanın temelini (konfigürasyon, loglama vb.) hazırlar. Bir nevi inşaata başlamadan önceki iskele kurulumudur.
 var builder = WebApplication.CreateBuilder(args);
+//appsettings.json dosyasındaki "JWTSettings" bölümünü okur. Token üretirken ve çözerken kullanacağın gizli anahtarı (Secret Key) buradan alacağız.
+var JwtSetting = builder.Configuration.GetSection("JWTSettings");
 
+//AddDbContext: Uygulamanın veritabanı ile konuşmasını sağlar.
+//UseSqlite: Hafif bir veritabanı olan SQLite kullanılacağını belirtir. Veritabanı dosyasının adı chat.db olacaktır.
 builder.Services.AddDbContext<AppDbContext>(x => x.UseSqlite("Data Source=chat.db"));
+
+//AddIdentityCore: .NET'in hazır kullanıcı yönetim sistemini (Login, Register işlemleri için) projeye ekler. AppUser senin özel kullanıcı sınıfındır.
+//AddEntityFrameworkStores: Kullanıcı verilerini (şifre, email vb.) yukarıda tanımladığın AppDbContext üzerinden veritabanında saklamasını söyler.
 builder.Services.AddIdentityCore<AppUser>()
 .AddEntityFrameworkStores<AppDbContext>()
 .AddDefaultTokenProviders();
+
+//AddAuthentication: Sisteme "Ben kimlik doğrulama yapacağım" der.
+builder.Services.AddAuthentication(opt =>
+{
+    opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme; //Sisteme, "Kullanıcıları tanımak için Cookie değil, JWT Token beklemelisin" kuralını koyar.
+    opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    opt.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(option =>
+{
+    option.SaveToken = true; // Token sunucuda saklansın mı? (Genelde false olabilir ama true da kalabilir)
+    option.RequireHttpsMetadata = false;
+    option.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true, // "İmza anahtarını mutlaka kontrol et" (Sahte tokenları engeller).
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes // Kontrol edilecek o gizli anahtar budur.
+        (JwtSetting.GetSection("SecurityKey").Value!)),
+        ValidateIssuer = false, // Token'ı kimin dağıttığını kontrol etme (Geliştirme aşamasında kolaylık).
+        ValidateAudience = false, // Token'ın kime verildiğini kontrol etme.
+    };
+});
+builder.Services.AddAuthorization();
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
@@ -18,11 +51,13 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.MapOpenApi(); // Sadece geliştirme modundaysan API dökümantasyonunu aktif et.
 }
 
-app.UseHttpsRedirection();
-app.UseAuthentication();
-app.UseAuthorization();
+app.UseHttpsRedirection(); // HTTP isteklerini zorla HTTPS'e çevirir (Güvenlik).
+app.UseAuthentication(); // 1. ÖNCE KİMLİK KONTROLÜ: "Sen kimsin? Token'ın geçerli mi?"
+app.UseAuthorization(); // 2. SONRA YETKİ KONTROLÜ: "Senin bu sayfaya girmeye iznin var mı?"
 
-app.Run();
+app.MapAccountEndpoint();
+
+app.Run(); // Uygulamayı başlat ve istekleri dinlemeye başla.
