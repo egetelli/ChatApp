f447eabb63af5bbe24bb9a9cf86641ac3fb84d73
Ege Telli <egetelli@hotmail.com>
2026-01-16 13:31:40 +0300

    feature(guard): guard eklendi

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,14 +1,17 @@
 import { Routes } from '@angular/router';
+import { loginGuard } from './guards/login.guard';
 
 export const routes: Routes = [
   {
     path: 'register',
     loadComponent: () =>
       import('./register/register.component').then((x) => x.RegisterComponent),
+    canActivate: [loginGuard],
   },
   {
     path: 'login',
     loadComponent: () =>
       import('./login/login.component').then((x) => x.LoginComponent),
-  }
+    canActivate: [loginGuard],
+  },
 ];
