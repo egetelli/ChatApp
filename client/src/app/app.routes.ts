fdecf6eb1a28cec3185b7c2346991b81812c8df9
Ege Telli <83495425+egetelli@users.noreply.github.com>
2026-01-16 13:59:23 +0300

    feature(chat-page-and-routes): chat component ve routeler eklendi (#12)

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,7 +1,14 @@
 import { Routes } from '@angular/router';
 import { loginGuard } from './guards/login.guard';
+import { ChatComponent } from './chat/chat.component';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
+  {
+    path: 'chat',
+    component: ChatComponent,
+    canActivate: [authGuard],
+  },
   {
     path: 'register',
     loadComponent: () =>
@@ -13,5 +20,10 @@ export const routes: Routes = [
     loadComponent: () =>
       import('./login/login.component').then((x) => x.LoginComponent),
     canActivate: [loginGuard],
+  },
+  {
+    path: '**',
+    redirectTo: 'chat',
+    pathMatch: 'full',
   },
 ];
