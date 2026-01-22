7bb342842ea11742087954b9415a6c368ec6967b
Ege Telli <egetelli@hotmail.com>
2026-01-22 17:11:14 +0300

    feature(auto-end-scroll): chatbox scrollun yeni mesajda en alta kaymasi yapildi

--- client/src/app/components/chat-window/chat-window.component.ts
+++ client/src/app/components/chat-window/chat-window.component.ts
@@ -1,23 +1,31 @@
 import { Component, inject } from '@angular/core';
 import { ChatService } from '../../services/chat.service';
 import { TitleCasePipe } from '@angular/common';
-import { MatIcon } from "@angular/material/icon";
+import { MatIcon } from '@angular/material/icon';
 import { FormsModule } from '@angular/forms';
-import { ChatBoxComponent } from "../chat-box/chat-box.component";
+import { ChatBoxComponent } from '../chat-box/chat-box.component';
 
 @Component({
   selector: 'app-chat-window',
   imports: [TitleCasePipe, MatIcon, FormsModule, ChatBoxComponent],
   templateUrl: './chat-window.component.html',
-  styles: ``
+  styles: ``,
 })
 export class ChatWindowComponent {
   chatService = inject(ChatService);
   message: string = '';
 
-  sendMessage(){
-    if(!this.message) return;
-    this.chatService.sendMessage(this.message);
+  sendMessage() {
+    // 1. Mesajın başındaki ve sonundaki gereksiz boşlukları temizle
+    const cleanMessage = this.message?.trim();
+
+    // 2. Eğer temizlenmiş hali boşsa (yani kullanıcı sadece space'e basmışsa) dur.
+    if (!cleanMessage) return;
+
+    // 3. Servise temizlenmiş mesajı gönder
+    this.chatService.sendMessage(cleanMessage);
+
+    // 4. Input kutusunu sıfırla
     this.message = '';
   }
 }
